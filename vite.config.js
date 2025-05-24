fa94581c18aca4fd6f18d47dc671a2826527a33c
Alex Alejo <areckstec@gmail.com>
2025-05-24 10:47:47 -0400

    mi pagina web subiendo v5

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'/arecks_website',
+  base:'/arecks.net',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
